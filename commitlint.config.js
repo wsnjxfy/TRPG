f9cbcca87a2ad1d97b3973de071a388513f7dc8f
wsnjxfy <Huxinyixinyibobo@126.com>
2025-12-26 16:17:28 +0800

    feat(前置工作): project structure setup

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能
+        'fix', // 修复bug
+        'docs', // 文档更新
+        'style', // 代码格式化
+        'refactor', // 重构
+        'test', // 测试相关
+        'chore', // 构建过程或工具更改
+        'perf', // 性能优化
+      ],
+    ],
+  },
+};
